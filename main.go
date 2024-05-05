337846ba9b9c9ec128af90cc8450483b36cb1a53
0num4 <r.oonuma@matsuri-tech.com>
2024-05-05 14:00:51 +0900

    feat: &analysis.Analyzer.Runにわたす関数とそのシンボルの定義を適切なものにした

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"golang.org/x/tools/go/analysis"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+}
+
+var a = &analysis.Analyzer{
+	Name:             "awesome analyzer",
+	Doc:              "",
+	URL:              "https://github.com/0num4",
+	Flags:            flag.FlagSet{},
+	Run:              Run,
+	RunDespiteErrors: false,
+	Requires:         nil,
+	ResultType:       nil,
+	FactTypes:        nil,
+}
+
+//func New(conf any) []*analysis.Analyzer {
+//	analyzer := &analysis.Analyzer{}
+//	// return analyzer
+//}
+//
+//func Test(t *testing.T) {
+//	testdata := analysistest.TestData()
+//	analysistest.Run(t, testdata)
+//}
+
+func Run(pass *analysis.Pass) (any, error) {
+	// Run the analyzer
+	fmt.Println("Running the analyzer")
+	return nil, nil
 }
--- readme.md
+++ readme.md
@@ -10,3 +10,14 @@ TBD
 
 https://egawata.hatenablog.com/entry/2021/08/29/005012
 https://disaev.me/p/writing-useful-go-analysis-linter/
+
+## go/analysis について
+
+golang.org/x/tools のツールの一つ。
+
+https://zenn.dev/tenntenn/books/d168faebb1a739/viewer/9d590e
+ソースコードを parse して AST を取得する
+
+## x/tools/go/analysisのanalysis.Analyzer構造体の使い方
+Runにanalyzerのfunctionを渡す
+
